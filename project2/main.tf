fbac00e82d7e87dee60a04eab38cd6a49e7b1d3a
root <root@ip-172-31-46-227.us-east-2.compute.internal>
2020-05-01 18:13:34 +0000

    Project2 second commit

---
+++ project2/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "dev" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "demo_instance"
+  }
+}
---
+++ project2/variables.tf
@@ -0,0 +1,7 @@
+variable "ami_id" {
+  default = "ami-07c1207a9d40bc3bd"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
