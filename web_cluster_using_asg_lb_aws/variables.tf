fd0ca0159531813c2376fc476f062a4cf2563db1
root <root@ip-172-31-46-227.us-east-2.compute.internal>
2020-05-05 18:44:24 +0000

    Web Cluster

---
+++ web_cluster_using_asg_lb_aws/variables.tf
@@ -0,0 +1,38 @@
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+
+
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "terraform-asg-example"
+}
+
+
+
+variable "instance_security_group_name" {
+  description = "The name of the security group for the EC2 Instances"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+
+
+variable "alb_security_group_name" {
+  description = "The name of the security group for the ALB"
+  type        = string
+  default     = "terraform-example-alb"
+}
+
+variable "image_id" {
+  default = "ami-07c1207a9d40bc3bd"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
